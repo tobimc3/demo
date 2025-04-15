9533d39ffcbf8350b55ab097029f059adf639754
Tobi McFarland <tobimc3@gmail.com>
2025-04-15 12:46:45 -0400

    Create JobControllerTest.java
    added JobControllerTest

---
+++ src/test/java/com/mcfarland/demo/JobControllerTest.java
@@ -0,0 +1,34 @@
+package com.mcfarland.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.isNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JobControllerTest {
+    
+    @Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getJobs() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/api/jobs").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+
+        
+                
+                
+	}
+
+
+}
