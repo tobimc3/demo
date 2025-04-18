0a7ac1440614d8a7c9d75fb2fd88ac0843c89a68
Tobi McFarland <tobimc3@gmail.com>
2025-04-18 16:34:01 -0400

    Create HelloControllerTest.java
    just asked co-pilot to generate this test class.  really cool

---
+++ src/test/java/com/mcfarland/demo/controller/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.mcfarland.demo.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+// File: HelloControllerTest.java
+package com.mcfarland.demo.controller;
+
+
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testIndex() throws Exception {
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Greetings from Spring Boot!"));
+	}
+}
