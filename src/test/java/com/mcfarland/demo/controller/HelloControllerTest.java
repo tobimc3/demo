ef36773ee7e1e065b03b7222614c88ab83924c27
Tobi McFarland <tobimc3@gmail.com>
2025-04-18 16:35:17 -0400

    Update HelloControllerTest.java

--- src/test/java/com/mcfarland/demo/controller/HelloControllerTest.java
+++ src/test/java/com/mcfarland/demo/controller/HelloControllerTest.java
@@ -8,8 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-// File: HelloControllerTest.java
-package com.mcfarland.demo.controller;
+
 
 
 
